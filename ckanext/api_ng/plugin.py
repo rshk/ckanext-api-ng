97c4981d0d20fc8c5566c0ac8250b611980288de
Samuele Santi <redshadow@hackzine.org>
2014-01-10 17:39:49 +0100

    More work in progress, new controller for the API

--- ckanext/api_ng/plugin.py
+++ ckanext/api_ng/plugin.py
@@ -1,66 +1,45 @@
 # -*- coding: utf-8 -*-
 
+##------------------------------------------------------------
 ## Ckan New Generation API for Ckan 2.2
-
-# try:
-#     from collections import OrderedDict
-# except ImportError:
-#     from ordereddict import OrderedDict
+##------------------------------------------------------------
 
 from ckan.plugins import implements, SingletonPlugin, IRoutes
-# import ckan.lib.base as base
-# import ckan.plugins.toolkit as plugins_toolkit
 import routes.mapper as routes_mapper
 
-from ckanext.api_ng.api_controller import ApiNgController
-
-
-## We would put this in the main ckan configuration, but
-## apparently routes are generated *before* configuration
-## is read, so that's not possible.. :(
-API_NG_URL_PREFIX = '/api/ng/'
+from ckanext.api_ng.api_controller import ApiNgController  # noqa
 
 
 class ApiNgPlugin(SingletonPlugin):
-    # implements(IConfigurable)
     implements(IRoutes)
-
-    ## Implementation of IConfigurable
-    ##------------------------------------------------------------
-
-    # def configure(self, config):
-    #     ## Retrieve custom URL prefix
-    #     self.conf = {
-    #         'url_prefix': config.get('api_ng.url_prefix', '/api/ng/'),
-    #     }
 
     ## Implementation of IRoutes
     ##------------------------------------------------------------
 
-    def _get_full_path(self, path):
-        url_parts = filter(None, API_NG_URL_PREFIX.split('/'))
-        parts = filter(None, path.split('/'))
-        return '/' + '/'.join(url_parts + parts)
+    @property
+    def _api_ng_url_prefix(self):
+        from pylons import config
+        return config.get('api_ng.base_url', '/api/ng/')
+
+    # def _get_full_path(self, path):
+    #     all_parts = self._api_ng_url_prefix.split('/') + path.split('/')
+    #     return '/' + '/'.join(filter(None, all_parts))
 
     def before_map(self, routes):
-        controller = 'ckanext.api_ng.plugin:ApiNgController'
+        controller = 'ckanext.api_ng.api_controller:ApiNgController'
 
-        #-------------------------------------------------------------------------------- # noqa
+        #----- config/routing.py -------------------------------------------------------- # noqa
         # 120     with SubMapper(map, controller='api', path_prefix='/api{ver:/3|}',      # noqa
         # 121                    ver='/3') as m:
         # 122         m.connect('/action/{logic_function}', action='action',
         # 123                   conditions=GET_POST)
         #-------------------------------------------------------------------------------- # noqa
 
-        ## This should do (pretty much)
         with routes_mapper.SubMapper(routes, controller=controller) as m:
             #m.connect('/api/ng/{path_info:.*}', action='action')
-            m.connect('/api/ng/{logic_function}', action='action')
+            # m.connect(self._api_ng_url_prefix + '{logic_function}/{extra:.*}') # noqa
+            m.connect(self._api_ng_url_prefix + '{action}')
 
-            # for name in ['package_list', 'package_get']:
-            #     page_slug = name.replace('_', '-')
-            #     page_path = self._get_full_path(page_slug)
-            #     m.connect(name, page_path, action=name)
         return routes
 
     def after_map(self, routes):
