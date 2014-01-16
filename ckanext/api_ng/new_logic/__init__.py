d9de8b025109225f7d12e9f229045cb704f875fe
Samuele Santi <redshadow@hackzine.org>
2014-01-16 17:11:02 +0100

    Added a bunch of functions to directly access the database, if needed

--- ckanext/api_ng/new_logic/__init__.py
+++ ckanext/api_ng/new_logic/__init__.py
@@ -1,4 +1,5 @@
 import collections
+import uuid
 
 from ckan.common import c
 import ckan.model
@@ -44,3 +45,50 @@ class LazyContext(collections.Mapping):
 
     def __len__(self):
         return len(self._context)
+
+
+def get_db_connection_params():
+    """
+    Parse the SQLAlchemy DB URL into a dictionary
+    suitable for psycopg2 connection.
+    """
+    from pylons import config
+    import urlparse
+
+    db_url = config.get('sqlalchemy.url')
+    db_url_parts = urlparse.urlparse(db_url)
+    if db_url_parts.scheme != 'postgresql':
+        raise ValueError("Invalid database scheme: " + db_url_parts.scheme)
+    user_pass, host_port = db_url_parts.netloc.split('@')
+    user, password = user_pass.split(':')
+    if ':' in host_port:
+        host, port = host_port.split(':')
+        port = int(port)
+    else:
+        host, port = host_port, 5432
+    dbname = db_url_parts.path.strip('/').split('/')[0]
+    return dict(
+        database=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port)
+
+
+def get_db():
+    """Get connection to the main database"""
+    ## todo: use connection pooling
+    import psycopg2
+    params = get_db_connection_params()
+    return psycopg2.connect(**params)
+
+
+def get_db_cursor():
+    """Return a DictCursor on the main database"""
+    import psycopg2.extras
+    return get_db().cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+def generate_id():
+    """Return an UUID suitable for use in database"""
+    return str(uuid.uuid4())
